6eca450cbffc49fe8c5757345098206faf484716
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-09-03 16:55:20 -0400

    Implementa la validación de la posición inicial para un caso correcto

--- src/controladorAuto.js
+++ src/controladorAuto.js
@@ -7,7 +7,14 @@ class ControladorAuto{
   }
 
   validarPosicionInicial(comandos){
-    return "El formato de la posición inicial es incorrecto. Debe ser 'X,YD' donde X y Y son números y D es una dirección (N, E, O, S).";
+    const formatoValido = /^\d+,\d+[NEOSneos]$/;
+    const partes = comandos.split('/');
+    const posicionInicial = partes[1];
+    
+    if (!formatoValido.test(posicionInicial)) {
+      return "El formato de la posición inicial es incorrecto. Debe ser 'X,YD' donde X y Y son números y D es una dirección (N, E, O, S).";
+    }
+    return "Formato válido.";
   }
 }
 
--- src/controladorAuto.test.js
+++ src/controladorAuto.test.js
@@ -26,5 +26,13 @@ describe("Validar que la posicion inicial siga el formato de la cadena -> mostra
     const mensaje = "El formato de la posición inicial es incorrecto. Debe ser 'X,YD' donde X y Y son números y D es una dirección (N, E, O, S).";
     expect(resultado).toEqual(mensaje);
   });
+
+  it("Deberia devolver un mensaje de aceptacion si el formato de la posicion inicial es correcto", () => {
+    
+    const comando = "5,5/1,2N/IAIAIAIAA";
+    const resultado = constroladorAuto.validarPosicionInicial(comando);
+    const mensaje = "Formato válido."
+    expect(resultado).toEqual(mensaje);
+  });
   
 });
